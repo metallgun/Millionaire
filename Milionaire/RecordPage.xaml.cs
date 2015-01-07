15afa1a4d726a26f3dfb1484a8a646a11739b6d3
Andrey <skittlzx@gmail.com>
2015-01-07 21:54:39 +0400

    FileNotFoundException fixd

--- Milionaire/RecordPage.xaml.cs
+++ Milionaire/RecordPage.xaml.cs
@@ -122,7 +122,7 @@ namespace Milionaire
 
                     }
                     }
-                    catch
+                    catch (FileNotFoundException)
                     {
                         FirstText.Text = "Рекордов пока нет";
                     }
